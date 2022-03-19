720aecff9c29c7ea7b3f1a0b047bf166723212d9
lkedron <lkedron@gmail.com>
2022-03-19 15:06:48 +0100

    additional validation

--- src/main/java/pl/dron1t/models/RequestBody.java
+++ src/main/java/pl/dron1t/models/RequestBody.java
@@ -4,11 +4,13 @@ import lombok.Data;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @Setter
 public class RequestBody {
 
-    @NotBlank(message = "{request.body.should.be.not.empty}")
+    @NotBlank(message = "{request.body.message.should.be.not.blank}")
+    @Size(min = 1, max = 256, message = "{request.body.message.size}")
     private String message;
 }
